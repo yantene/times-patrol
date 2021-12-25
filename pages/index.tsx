de651d0352cd9b7751ae21d088670487e652a286
Shuhei YOSHIDA <contact@yantene.net>
2021-12-26 00:13:56 +0900

    npm init frourio-app times-patrol && nodenv local 14.18.2

---
+++ pages/index.tsx
@@ -0,0 +1,108 @@
+import Head from 'next/head'
+import { useCallback, useState } from 'react'
+import { useQueryClient } from 'react-query'
+import { useAspidaQuery } from '@aspida/react-query'
+import styles from '~/styles/Home.module.css'
+import { apiClient } from '~/utils/apiClient'
+import UserBanner from '~/components/UserBanner'
+import type { Task } from '$prisma/client'
+import type { FormEvent, ChangeEvent } from 'react'
+
+const Home = () => {
+  const queryClient = useQueryClient()
+  const { data: tasks, error } = useAspidaQuery(apiClient.tasks)
+  const [label, setLabel] = useState('')
+  const inputLabel = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => setLabel(e.target.value),
+    []
+  )
+
+  const invalidateTasks = useCallback(
+    () => queryClient.invalidateQueries(apiClient.tasks.$path()),
+    [queryClient]
+  )
+
+  const createTask = useCallback(
+    async (e: FormEvent) => {
+      e.preventDefault()
+      if (!label) return
+
+      await apiClient.tasks.post({ body: { label } })
+      setLabel('')
+      invalidateTasks()
+    },
+    [label]
+  )
+
+  const toggleDone = useCallback(async (task: Task) => {
+    await apiClient.tasks._taskId(task.id).patch({ body: { done: !task.done } })
+    invalidateTasks()
+  }, [])
+
+  const deleteTask = useCallback(async (task: Task) => {
+    await apiClient.tasks._taskId(task.id).delete()
+    invalidateTasks()
+  }, [])
+
+  if (error) return <div>failed to load</div>
+  if (!tasks) return <div>loading...</div>
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>frourio-todo-app</title>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+
+      <main className={styles.main}>
+        <UserBanner />
+
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+
+        <p className={styles.description}>frourio-todo-app</p>
+
+        <div>
+          <form style={{ textAlign: 'center' }} onSubmit={createTask}>
+            <input value={label} type="text" onChange={inputLabel} />
+            <input type="submit" value="ADD" />
+          </form>
+          <ul className={styles.tasks}>
+            {tasks.map((task) => (
+              <li key={task.id}>
+                <label>
+                  <input
+                    type="checkbox"
+                    checked={task.done}
+                    onChange={() => toggleDone(task)}
+                  />
+                  <span>{task.label}</span>
+                </label>
+                <input
+                  type="button"
+                  value="DELETE"
+                  style={{ float: 'right' }}
+                  onClick={() => deleteTask(task)}
+                />
+              </li>
+            ))}
+          </ul>
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </div>
+  )
+}
+
+export default Home
