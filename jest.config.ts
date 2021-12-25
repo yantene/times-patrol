89806f7efeab993100f4ddf1289dc307bc4dc868
Shuhei YOSHIDA <contact@yantene.net>
2021-12-25 16:49:15 +0000

    Remove .prettierrc and Run lint:fix

--- jest.config.ts
+++ jest.config.ts
@@ -1,33 +1,33 @@
-import { pathsToModuleNameMapper } from 'ts-jest/utils'
-import type { Config } from '@jest/types'
-import { compilerOptions } from './tsconfig.json'
+import { pathsToModuleNameMapper } from "ts-jest/utils";
+import type { Config } from "@jest/types";
+import { compilerOptions } from "./tsconfig.json";
 
 const config: { projects: Config.InitialOptions[] } = {
   projects: [
     {
-      setupFiles: ['<rootDir>/test/jest.setup.js'],
-      testPathIgnorePatterns: ['<rootDir>/server'],
+      setupFiles: ["<rootDir>/test/jest.setup.js"],
+      testPathIgnorePatterns: ["<rootDir>/server"],
       transform: {
-        '^.+\\.tsx$': 'babel-jest',
-        '^.+\\.ts$': 'ts-jest'
+        "^.+\\.tsx$": "babel-jest",
+        "^.+\\.ts$": "ts-jest",
       },
       moduleNameMapper: {
-        '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
-        '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
+        "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+        "\\.(gif|ttf|eot|svg|png)$": "<rootDir>/test/__mocks__/fileMock.js",
         ...pathsToModuleNameMapper(compilerOptions.paths, {
-          prefix: '<rootDir>/'
-        })
-      }
+          prefix: "<rootDir>/",
+        }),
+      },
     },
     {
-      preset: 'ts-jest',
-      testEnvironment: 'node',
-      testMatch: ['<rootDir>/server/test/**/*.ts'],
+      preset: "ts-jest",
+      testEnvironment: "node",
+      testMatch: ["<rootDir>/server/test/**/*.ts"],
       moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
-        prefix: '<rootDir>/'
-      })
-    }
-  ]
-}
+        prefix: "<rootDir>/",
+      }),
+    },
+  ],
+};
 
-export default config
+export default config;
--- utils/apiClient.ts
+++ utils/apiClient.ts
@@ -1,4 +1,4 @@
-import aspida from '@aspida/fetch'
-import api from '~/server/api/$api'
+import aspida from "@aspida/fetch";
+import api from "~/server/api/$api";
 
-export const apiClient = api(aspida(undefined, { throwHttpErrors: true }))
+export const apiClient = api(aspida(undefined, { throwHttpErrors: true }));
